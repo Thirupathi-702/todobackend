4907ebd707e856428d51a0df71321a2bf3d814d2
Thirupathi-702 <21r05a6702@gmail.com>
2024-06-28 09:08:37 +0530

    df

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express=require('express')
-const mongoose=require("mongoose");
-const app=express();
-const port=3000;
-const authRoutes=require("./routes/auth")
-const todoRoutes=require("./routes/todo")
-const cors=require("cors")
+require('dotenv').config();
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT || 3000;
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use("/auth",authRoutes);
-app.use("/todo",todoRoutes);
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
 
-app.listen(port,()=>{
-    console.log(`example app listening at http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`example app listening at http://localhost:${port}`);
+});
 
-})
-mongoose.connect("mongodb+srv://gudithirupathi02:thiru@database01.qe0b4xx.mongodb.net/?retryWrites=true&w=majority&appName=database01");
+mongoose.connect(process.env.MONGO_URI//, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+)
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
